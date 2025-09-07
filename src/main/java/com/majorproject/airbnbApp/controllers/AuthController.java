5774ca716b2b15fcfc6e2de5b8cd914e3954c215
Rohan Uke <ukerohan7@gmail.com>
2025-09-07 22:14:38 +0530

    Add Auth Controller To SignUp Login Requested and Generates JWT to Further Authentication According To Role

---
+++ src/main/java/com/majorproject/airbnbApp/controllers/AuthController.java
@@ -0,0 +1,56 @@
+package com.majorproject.airbnbApp.controllers;
+
+import com.majorproject.airbnbApp.dtos.LoginDto;
+import com.majorproject.airbnbApp.dtos.LoginResponseDto;
+import com.majorproject.airbnbApp.dtos.SignUpRequestDto;
+import com.majorproject.airbnbApp.dtos.UserDto;
+import com.majorproject.airbnbApp.security.Auth_Service;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+    private final Auth_Service authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserDto> signup(@RequestBody SignUpRequestDto signUpRequestDto) {
+        return new ResponseEntity<>(authService.signUp(signUpRequestDto), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginDto loginDto, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        String[] tokens = authService.login(loginDto);
+
+        Cookie cookie = new Cookie("refreshToken", tokens[1]);
+        cookie.setHttpOnly(true);
+
+        httpServletResponse.addCookie(cookie);
+        return ResponseEntity.ok(new LoginResponseDto(tokens[0]));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponseDto> refresh(HttpServletRequest request) {
+        String refreshToken = Arrays.stream(request.getCookies()).
+                filter(cookie -> "refreshToken".equals(cookie.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new AuthenticationServiceException("Refresh token not found inside the Cookies"));
+
+        String accessToken = authService.refreshToken(refreshToken);
+        return ResponseEntity.ok(new LoginResponseDto(accessToken));
+    }
+
+}
